a903aa1dcda0fb5b152085b13df4a4e325efd42b
youngancient <tochyokoye@gmail.com>
2024-10-19 02:32:20 +0100

    working on tests

--- test/deployDiamond.t.sol
+++ test/deployDiamond.t.sol
@@ -5,6 +5,8 @@ import "../contracts/interfaces/IDiamondCut.sol";
 import "../contracts/facets/DiamondCutFacet.sol";
 import "../contracts/facets/DiamondLoupeFacet.sol";
 import "../contracts/facets/OwnershipFacet.sol";
+import "../contracts/facets/ERC721Facet.sol";
+import "../contracts/facets/MerkleAirdropFacet.sol";
 import "../contracts/Diamond.sol";
 
 import "./helpers/DiamondUtils.sol";
@@ -15,18 +17,24 @@ contract DiamondDeployer is DiamondUtils, IDiamondCut {
     DiamondCutFacet dCutFacet;
     DiamondLoupeFacet dLoupe;
     OwnershipFacet ownerF;
+    ERC721Facet erc721F;
+    MerkleAirdropFacet merkleF;
 
     function testDeployDiamond() public {
+        address owner = address(0x123);
+        vm.startPrank(owner);
+
         //deploy facets
         dCutFacet = new DiamondCutFacet();
-        diamond = new Diamond(address(this), address(dCutFacet));
+        diamond = new Diamond(owner, address(dCutFacet));   // add owner as owner
         dLoupe = new DiamondLoupeFacet();
         ownerF = new OwnershipFacet();
-
+        erc721F = new ERC721Facet();
+        merkleF = new MerkleAirdropFacet();
         //upgrade diamond with facets
 
         //build cut struct
-        FacetCut[] memory cut = new FacetCut[](2);
+        FacetCut[] memory cut = new FacetCut[](4);
 
         cut[0] = (
             FacetCut({
@@ -44,11 +52,44 @@ contract DiamondDeployer is DiamondUtils, IDiamondCut {
             })
         );
 
+        cut[2] = (
+            FacetCut({
+                facetAddress: address(ownerF),
+                action: FacetCutAction.Add,
+                functionSelectors: generateSelectors("ERC721Facet")
+            })
+        );
+
+        cut[3] = (
+            FacetCut({
+                facetAddress: address(ownerF),
+                action: FacetCutAction.Add,
+                functionSelectors: generateSelectors("MerkleAirdropFacet")
+            })
+        );
+
         //upgrade diamond
         IDiamondCut(address(diamond)).diamondCut(cut, address(0x0), "");
 
         //call a function
         DiamondLoupeFacet(address(diamond)).facetAddresses();
+        
+         // Initialize the ERC721Facet
+        ERC721Facet(address(diamond)).initialize("TestToken", "TTK");
+
+
+        address to = address(0x246);
+
+        // Test minting an NFT
+        ERC721Facet(address(diamond)).mint(to, 1);
+        assertEq(ERC721Facet(address(diamond)).ownerOf(1), to);
+        assertEq(ERC721Facet(address(diamond)).balanceOf(to), 1);
+
+        // Test transferring an NFT
+        // ERC721Facet(address(diamond)).transferFrom(address(this), address(0x1), 1);
+        // assertEq(ERC721Facet(address(diamond)).ownerOf(1), address(0x1));
+        // assertEq(ERC721Facet(address(diamond)).balanceOf(address(this)), 0);
+        // assertEq(ERC721Facet(address(diamond)).balanceOf(address(0x1)), 1);
     }
 
     function test_Fn() public{
